2f932d3c34ff7f68e5e309493c938a21db7db8fd
viniguima <guimavinicius99@gmail.com>
2022-09-15 00:57:00 -0300

    update

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Button } from "@react-native-material/core";
+// import { global_styles } from 'src/styles/global-styles'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}> 
+      <Image
+        style={styles.tinyLogo}
+        source={require('public/images/logo-branca.png')}
+      /> 
+      <Text style={styles.white_text}>Clique se concorda</Text>
+      <Button title="Bosta" style={styles.white_text}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2a3540',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  white_text: {
+    color: 'white',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
   },
 });
---
+++ src/styles/global-styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#2a3540',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
+
+const global_styles = styles;
+
+export default { global_styles };
+export { global_styles }
