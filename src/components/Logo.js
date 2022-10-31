3a9aa14b6617cebab3f4805577e38bb11809c585
imLeonam <leonammrib@gmail.com>
2022-10-31 19:23:20 -0300

    feat(icons): added new icons in the public images

---
+++ src/components/Logo.js
@@ -0,0 +1,20 @@
+import { View, Image, StyleSheet } from 'react-native';
+
+export default function Logo() {
+  return (
+    <View>
+      <Image
+        style={styles.tinyLogo}
+        source={require('../../public/images/logo-branca.png')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  tinyLogo: {
+    width: 280,
+    height: 120,
+    marginTop: 20
+  },
+});
